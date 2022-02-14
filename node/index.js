fd6a9be300c97d630dab285025107c221c6138a5
joris-villet <jorisvillet@gmail.com>
2022-02-14 21:53:47 +0100

    add h1 in nodejs app res.send

--- node/index.js
+++ node/index.js
@@ -4,7 +4,11 @@ const app = express();
 require('dotenv').config();
 
 app.get('/', (req, res) => {
-  res.send(`Hello from Docker-compose this is a nodejs app, the nodejs version is: ${process.version} !!`)
+  res.send(`
+  <h1 style="margin: 2rem auto; text-align: center">
+    Hello from Docker-compose this is a nodejs app, the nodejs version is: ${process.version} !!
+  </h1>
+  `)
 })
 
 const port = process.env.PORT;
